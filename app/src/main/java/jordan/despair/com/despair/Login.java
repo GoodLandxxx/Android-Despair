5c592fe78a9b71746a3d0b1d958bedd65a7644f5
TuQiong <987331847@qq.com>
2016-10-24 13:31:36 +0800

    Rejected commit: Default

--- app/src/main/java/jordan/despair/com/despair/Login.java
+++ app/src/main/java/jordan/despair/com/despair/Login.java
@@ -2,6 +2,8 @@ package jordan.despair.com.despair;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView textView = (TextView) findViewById(R.id.title_text);
+        textView.setText("登录");
+
     }
 }
